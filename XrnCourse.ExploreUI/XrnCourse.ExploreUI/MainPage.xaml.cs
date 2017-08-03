99678e6c16e3daa9b30e1c8b0f20f584d66e2ced
sigged <sderdeyn@gmail.com>
2017-08-03 13:10:19 +0200

    Example usage of OnPlatform and OnIdiom

--- XrnCourse.ExploreUI/XrnCourse.ExploreUI/MainPage.xaml.cs
+++ XrnCourse.ExploreUI/XrnCourse.ExploreUI/MainPage.xaml.cs
@@ -12,6 +12,29 @@ namespace XrnCourse.ExploreUI
         public MainPage()
         {
             InitializeComponent();
+
+
+            Label label = new Label();
+            var pfText = new OnPlatform<string>();
+            pfText.Platforms.Add(new On { Platform = new List<string> { "iOS" }, Value = "Apples", });
+            pfText.Platforms.Add(new On { Platform = new List<string> { "Android" }, Value = "Bots" });
+            pfText.Platforms.Add(new On { Platform = new List<string> { "Windows" }, Value = "Windows" });
+            label.Text = pfText;
+            myLayout.Children.Add(label);
+
+            switch(Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    label.BackgroundColor = Color.LightBlue;
+                    break;
+                case Device.Android:
+                    label.BackgroundColor = Color.LightGreen;
+                    break;
+                default: //windows, winphone and all others
+                    label.BackgroundColor = Color.Wheat;
+                    break;
+            }
+
         }
     }
 }
